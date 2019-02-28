0cf55f055f585ef5126bbd1944b2224b41364c36
Thomas Monfre <tmonfre@users.noreply.github.com>
2019-02-27 23:53:35 -0500

    updated pooled estimate for standard error

--- gov10_coffee_study.R
+++ gov10_coffee_study.R
@@ -28,7 +28,27 @@ m = length(kaf_last$Subject_Number)
 kaf_first_accuracy = length(kaf_first$Correct_Guess[kaf_first$Correct_Guess == 1]) / n
 kaf_last_accuracy = length(kaf_last$Correct_Guess[kaf_last$Correct_Guess == 1]) / m
 
-# get margins of error individually (in a one sample test)
+# get average cups per day
+kaf_first_avg_cpd = mean(kaf_first$Avg_Cups_Per_Day)
+kaf_last_avg_cpd = mean(kaf_last$Avg_Cups_Per_Day)
+
+# get average class year for each group
+for (group in unique(kaf_first$Class_Year)) {
+    print(group)
+    print(paste("KAF First: ", length(kaf_first$Class_Year[kaf_first$Class_Year == group])))
+    print(paste("KAF Last: ", length(kaf_last$Class_Year[kaf_last$Class_Year == group])))
+    print("--------------------")
+}
+
+# get average class year for each group
+for (group in unique(kaf_first$Preferred_Location_Summarized)) {
+  print(group)
+  print(paste("KAF First: ", length(kaf_first$Preferred_Location_Summarized[kaf_first$Preferred_Location_Summarized == group])))
+  print(paste("KAF Last: ", length(kaf_last$Preferred_Location_Summarized[kaf_last$Preferred_Location_Summarized == group])))
+  print("--------------------")
+}
+
+# get margins of error individually (in a one sample test) -- not sure if we need this
 kaf_first_moe = z_star * sqrt((kaf_first_accuracy*(1 - kaf_first_accuracy)/n))
 kaf_last_moe = z_star * sqrt((kaf_last_accuracy*(1 - kaf_last_accuracy)/m))
 
@@ -40,14 +60,13 @@ point_estimate = kaf_first_accuracy - kaf_last_accuracy
 margin_of_error = z_star * sqrt( ((kaf_first_accuracy*(1-kaf_first_accuracy))/length(kaf_first$Subject_Number)) + ((kaf_last_accuracy*(1-kaf_last_accuracy))/length(kaf_last$Subject_Number)) )
 paste(point_estimate, "±", margin_of_error)
 
-# compute test-statistic
-pnm = ((kaf_first_accuracy + kaf_last_accuracy)/(n + m))
-test_statistic = (kaf_first_accuracy - kaf_last_accuracy - 0) / sqrt(pnm*(1-pnm)*(1/n+1/m))
-
+# compute test-statistic and p-value
+pnm = (length(kaf_first$Correct_Guess[kaf_first$Correct_Guess == 1]) + length(kaf_last$Correct_Guess[kaf_last$Correct_Guess == 1]))/(n + m)
+test_statistic = (kaf_first_accuracy - kaf_last_accuracy - 0) / sqrt(pnm*(1-pnm)*((1/n)+(1/m)))
 p_value = 2*pnorm(-1*test_statistic)
 
 # plot test statistic on standard normal curve
-curve(dnorm(x), xlim=c(-5,5), main='Standard Normal', xaxt = "n") 
+curve(dnorm(x), xlim=c(-5,5), main='Sampling Distribution of Test Statistic', xlab="", ylab="", xaxt = "n") 
 axis(side = 1, at = c(-1.96, 1.96), labels=c(-1.96, 1.96))
 
 # add rejection region to left
